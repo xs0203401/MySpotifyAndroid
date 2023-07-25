04234ef33f897124ca004b85458fb7bb80c3036f
henryliu <henryliuyaohuang@gmail.com>
2023-07-24 22:41:56 -0500

    HomeScreen AlbumCover

--- app/src/main/java/com/laioffer/spotify/ui/home/HomeScreen.kt
+++ app/src/main/java/com/laioffer/spotify/ui/home/HomeScreen.kt
@@ -1,18 +1,23 @@
 package com.laioffer.spotify.ui.home
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.laioffer.spotify.R
+import com.laioffer.spotify.datamodel.Album
 import com.laioffer.spotify.datamodel.Section
 
 @Composable
@@ -55,7 +60,42 @@ private fun AlbumSection(section: Section) {
             color = Color.White
         )
     }
+    LazyRow(
+        modifier = Modifier.padding(top = 8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(section.albums) { item ->
+            AlbumCover(item)
+        }
+    }
+}
 
+@Composable
+private fun AlbumCover(album: Album) {
+    Column {
+        Box(modifier = Modifier.size(160.dp)) {
+            AsyncImage(
+                model = album.cover,
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.FillBounds
+            )
+            Text(
+                text = album.name,
+                color = Color.White,
+                modifier = Modifier
+                    .padding(bottom = 4.dp, start = 2.dp)
+                    .align(Alignment.BottomStart),
+            )
+        }
+
+        Text(
+            text = album.artists,
+            modifier = Modifier.padding(top = 4.dp),
+            style = MaterialTheme.typography.body2.copy(fontWeight = FontWeight.Bold),
+            color = Color.LightGray,
+        )
+    }
 }
 
 @Composable
