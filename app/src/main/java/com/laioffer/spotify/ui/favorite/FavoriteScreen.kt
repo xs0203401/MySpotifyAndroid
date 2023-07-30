e88a6c12fd5dfdd777bc07924b5ebb00689548b4
henryliu <henryliuyaohuang@gmail.com>
2023-07-30 02:38:06 -0500

    FavoriteScreen

---
+++ app/src/main/java/com/laioffer/spotify/ui/favorite/FavoriteScreen.kt
@@ -0,0 +1,9 @@
+package com.laioffer.spotify.ui.favorite
+
+import androidx.compose.runtime.Composable
+import com.laioffer.spotify.datamodel.Album
+
+@Composable
+fun FavoriteScreen(viewModel: FavoriteViewModel, onTap: (Album) -> Unit) {
+
+}
