7af698da83d7360593ab727c71187629a56a99b8
henryliu <henryliuyaohuang@gmail.com>
2023-08-01 02:10:12 -0500

    Player Bar with Seek Bar

--- app/src/main/java/com/laioffer/spotify/MainActivity.kt
+++ app/src/main/java/com/laioffer/spotify/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.darkColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
@@ -19,6 +22,8 @@ import com.laioffer.spotify.database.DatabaseDao
 import com.laioffer.spotify.datamodel.Album
 import com.laioffer.spotify.network.NetworkApi
 import com.laioffer.spotify.network.NetworkModule
+import com.laioffer.spotify.player.PlayerBar
+import com.laioffer.spotify.player.PlayerViewModel
 import com.laioffer.spotify.ui.theme.SpotifyTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
@@ -34,6 +39,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var api: NetworkApi
     @Inject
     lateinit var databaseDao: DatabaseDao
+    private val playerViewModel: PlayerViewModel by viewModels() // Activity Scope
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,6 +62,18 @@ class MainActivity : AppCompatActivity() {
             NavigationUI.onNavDestinationSelected(menuItem, navController)
             navController.popBackStack(menuItem.itemId, inclusive = false)
             true
+        }
+
+
+        val playerBar = findViewById<ComposeView>(R.id.player_bar)
+        playerBar.apply {
+            setContent {
+                MaterialTheme(colors = darkColors()) {
+                    PlayerBar(
+                        playerViewModel
+                    )
+                }
+            }
         }
 
         // Test retrofit
